d4ba9685a0ed377693e67778c09d2b73ff881ccb
Void-Frog1 <146322102+Void-Frog1@users.noreply.github.com>
2023-11-08 12:22:46 +0300

    Update laba4.cpp

--- laba4.cpp
+++ laba4.cpp
@@ -6,30 +6,34 @@ int main()
 {
 	setlocale(0, "");
 
-	const int DLYPOPA = 15;
+	const int ARRSIZE = 15;
 
-	int arr[DLYPOPA]{
+	int arr[ARRSIZE]{
 		4, 1, 3, -4, 4, 8, 43, 23, 5, 5, 6, 8, 33, 6, 6
 	};
 
 	int minNum = 0;
 	int maxNum = 0;
+	int minNumIndex = 0;
+	int maxNumIndex = 0;
 
-	for (int i = 0; i < DLYPOPA; i++)
+	for (int i = 0; i < ARRSIZE; i++)
 	{
 		if (arr[i] < minNum || i == 0)
 		{
+			minNumIndex = i;
 			minNum = arr[i];
 		}
 
 		if (arr[i] > maxNum || i == 0)
 		{
+			maxNumIndex = i;
 			maxNum = arr[i];
 		}
 
 	}
 
-	cout << "Минимальное число будет: " << minNum << endl;
-	cout << "Максимальное число будет: " << maxNum << endl;
+	cout << "Минимальное число будет: " << minNum << ",и его индекс" << minNumIndex << endl;
+	cout << "Максимальное число будет: " << maxNum << ",и его индекс" << maxNumIndex << endl;
 
 }
